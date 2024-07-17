0e5dbd528c3ddcff55125d6c506525185ddd321a
oalt <oa@lacheweg21.de>
2024-07-17 17:46:58 +0200

    UI improvements

--- src/MDD4All.DevOpsObserver.Views/StatusOverview.razor.cs
+++ src/MDD4All.DevOpsObserver.Views/StatusOverview.razor.cs
@@ -7,5 +7,18 @@ namespace MDD4All.DevOpsObserver.Views
     {
         [Parameter]
         public MainViewModel DataContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if(DataContext != null)
+            {
+                DataContext.PropertyChanged += OnPropertyChanged;
+            }
+        }
+
+        private void OnPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            StateHasChanged();
+        }
     }
 }
